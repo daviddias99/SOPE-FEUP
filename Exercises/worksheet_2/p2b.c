966c32f008dbee4f5acc8684323cb23c687db66e
David <up201705373@fe.up.pt>
2019-02-26 21:36:34 +0000

    Finished p2b and p2a (but still needs comments and error checking)

--- Exercises/worksheet_2/p2b.c
+++ Exercises/worksheet_2/p2b.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h> 
 
 #define CHUNK_SIZE 64
@@ -15,17 +16,16 @@ int main(int argc, char* argv[]){
 
 
     char chunk[CHUNK_SIZE];
-
-    int sourceFileFD = open(argv[1],O_RDONLY,S_IRUSR | S_IRGRP | S_IROTH);
-    int destFileFD= open(argv[1],O_WRONLY,S_IWUSR | S_IWGRP | S_IWOTH);
-
     ssize_t nRead;
+
+    int sourceFileFD = open(argv[1],O_RDONLY);
+    int destFileFD= open(argv[2],O_WRONLY | O_CREAT | O_EXCL,0644);
 
     if((sourceFileFD== -1) || (destFileFD == -1))
         return -2;
 
-    while( (nRead = read(sourceFileFD,chunk,CHUNK_SIZE)) != 0) {
-        write(destFileFD,chunk,CHUNK_SIZE);
+    while( (nRead = read(sourceFileFD,chunk,CHUNK_SIZE)) != 0) 
+        write(destFileFD,chunk,nRead);
 
     close(sourceFileFD);
     close(destFileFD);
