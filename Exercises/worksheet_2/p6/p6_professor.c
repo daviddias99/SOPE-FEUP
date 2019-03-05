308b27f64cd3bddd8bb6fcd59f4a77618f7f2c39
David <up201705373@fe.up.pt>
2019-03-05 19:04:38 +0000

    Did some testing in p6 of worksheet 2

--- Exercises/worksheet_2/p6/p6_professor.c
+++ Exercises/worksheet_2/p6/p6_professor.c
@@ -1,3 +1,5 @@
+/* LISTAR FICHEIROS REGULARES E SUB-DIRECTÓRIOS DE UM DIRECTÓRIO */
+/* USO: listdir2 dirname */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,35 +13,37 @@ int main(int argc, char *argv[])
     struct dirent *direntp;
     struct stat stat_buf;
     char *str;
-
+    char name[257];
     if (argc != 2)
     {
         fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
         exit(1);
     }
-
     if ((dirp = opendir(argv[1])) == NULL)
     {
         perror(argv[1]);
         exit(2);
     }
-
     while ((direntp = readdir(dirp)) != NULL)
     {
-        if (stat(direntp->d_name, &stat_buf) == -1) // testar com stat()
+        sprintf(name, "%s/%s", argv[1], direntp->d_name); // <----- NOTAR
+        // alternativa a chdir(); ex: anterior
+        if (lstat(name, &stat_buf) == -1)
+        // testar com stat()
         {
             perror("lstat ERROR");
             exit(3);
         }
+        //
+        printf("%10d - ", (int)stat_buf.st_ino);
         if (S_ISREG(stat_buf.st_mode))
             str = "regular";
         else if (S_ISDIR(stat_buf.st_mode))
             str = "directory";
         else
             str = "other";
-        printf("%-25s - %-15s |%-15d | %d\n", direntp->d_name, str, (int)stat_buf.st_ino, (int)stat_buf.st_size);
+        printf("%-25s - %s\n", direntp->d_name, str);
     }
-
     closedir(dirp);
     exit(0);
 }
