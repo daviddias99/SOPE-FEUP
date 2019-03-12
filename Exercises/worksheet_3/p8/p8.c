db5fa097d67200fdce7274d1bfbf5a9dc8f53cdf
David <up201705373@fe.up.pt>
2019-03-12 14:46:37 +0000

    Added p8

--- Exercises/worksheet_3/p8/p8.c
+++ Exercises/worksheet_3/p8/p8.c
@@ -17,8 +17,23 @@ int main(int argc, char *argv[], char *envp[])
         printf("My child is going to execute command\"ls -laR %s\"\n", argv[1]);
     else if (pid == 0)
     {
-        execlp("ls", "ls", "-laR", argv[1],NULL);
+        // execlp("ls", "ls", "-laR", argv[1],NULL);    //     8a
+        // execl("/bin/ls", "ls","-laR", argv[1],NULL); //     8b
+
+        /*
+        char* arguments[] = {"ls", "-laR", argv[1], NULL};
+        execvp("ls", arguments); *///                          8c
+
+        /*
+        char* arguments[] = {"ls", "-laR", argv[1], NULL};
+        execv("/bin/ls", arguments);  *///                    8d
+
+        char* arguments[] = {"ls", "-laR", argv[1], NULL};
+
+        execve("/bin/ls", arguments, envp); 
+
         printf("Command not executed !\n");
+        perror("Exec error");
         exit(1);
     }
     exit(0);
