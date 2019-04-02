143e5dafe5920b7abdc7ccfb2eba3f407e0cf993
David Silva <up201705373@fe.up.pt>
2019-04-02 10:54:19 +0100

    Added p2 of worksheet 5

--- Exercises/worksheet_5/p2/p2a.c
+++ Exercises/worksheet_5/p2/p2a.c
@@ -3,45 +3,106 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define READ            0
 #define WRITE           1
 
-struct operands {
-
-    int a;
-    int b;
-};
-
 
 int main(int argc, char* argv[]){
 
-    int fd[2]; 
-    pid_t pid;
-    struct operands ops;
+    int parent_fd[2]; 
+    int child_fd[2]; 
 
-    if(pipe(fd) == -1)
+    pid_t pid;
+
+    if(pipe(parent_fd) == -1)
+        exit(-1);
+
+    if(pipe(child_fd) == -1)
         exit(-1);
     
     pid = fork();
 
     if(pid > 0) {               //parent
 
-        close(fd[READ]);
-        scanf("%d%d",&ops.a,&ops.b);
-        write(fd[WRITE],&ops,sizeof(struct operands));
+        close(parent_fd[READ]);
+        close(child_fd[WRITE]);
+
+        int numA, numB;
+        scanf("%d%d",&numA,&numB);
+
+        write(parent_fd[WRITE],&numA,sizeof(int));
+        write(parent_fd[WRITE],&numB,sizeof(int));
+
+        while(true){
+
+            char currType;
+            int nRead = read(child_fd[READ],&currType,sizeof(char));
+
+            if(nRead == 0)
+                break;
+
+            if(currType == 'i'){
+
+                int currRes;
+                read(child_fd[READ],&currRes,sizeof(int));
+                printf("%d \n",currRes);
+
+            }
+            else if (currType == 'f'){
+
+                float currRes;
+                read(child_fd[READ],&currRes,sizeof(float));
+                printf("%f \n",currRes);
+
+            }
+            else if (currType == 'E'){
+
+                float currRes;
+                read(child_fd[READ],&currRes,sizeof(float));
+                printf("INVALIDO \n");
+            }
+
+        }
 
     } else if (pid == 0) {      //child
 
-        close(fd[WRITE]);
-        read(fd[READ],&ops,sizeof(struct operands));
+        close(parent_fd[WRITE]);
+        close(child_fd[READ]);
 
-        if(!ops.b)  return 1;
+        int numA, numB;
+        read(parent_fd[READ],&numA,sizeof(int));
+        read(parent_fd[READ],&numB,sizeof(int));
+
+        int sum = numA + numB;
+        int sub = numA - numB;
+        int mul = numA * numB;
+        float div = 0;
+        char divR;
+
+        if(numB != 0){
             
-        printf(" a + b = %d \n a - b = %d \n a * b = %d \n a / b = %f \n", ops.a + ops.b, ops.a - ops.b, ops.a * ops.b, (float)ops.a/ops.b);
+            divR = 'f';
+            div = (float)numA / numB;
+        }
+        else{
+            divR = 'E';
+        }
+
+        write(child_fd[WRITE],"i",sizeof(char));
+        write(child_fd[WRITE],&sum,sizeof(int));
+        write(child_fd[WRITE],"i",sizeof(char));
+        write(child_fd[WRITE],&sub,sizeof(int));
+        write(child_fd[WRITE],"i",sizeof(char));
+        write(child_fd[WRITE],&mul,sizeof(int));
+        write(child_fd[WRITE],&divR,sizeof(char));
+        write(child_fd[WRITE],&div,sizeof(float));
+        
+
+        //printf(" a + b = %d \n a - b = %d \n a * b = %d \n a / b = %f \n", numA + numB, numA - numB, numA * numB, (float)numA/numB);
 
         return 0;
     }
-    
     return 0;
 }
